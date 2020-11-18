1b06d2e236846253ca2d9cef59a34f246f9936cf
rarcz29 <rafalcz29@gmail.com>
2020-11-18 10:24:32 +0100

    Add projects to the solution

---
+++ Server/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
